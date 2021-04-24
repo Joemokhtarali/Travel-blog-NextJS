9acffe94ca35dd1dba84a3506212182527aa2137
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-24 20:41:45 +0200

    hero component & styled it

--- travel-blog-nextjs/components/home-page/hero.js
+++ travel-blog-nextjs/components/home-page/hero.js
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 function Hero() {
   return (
-   <div>
-       Hero 
-   </div>
-  )
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/Mokhtar.jpg"
+          alt="Mokhtar image"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Mokhtar</h1>
+      <p>I blog about Traveling</p>
+    </section>
+  );
 }
 
 export default Hero;
