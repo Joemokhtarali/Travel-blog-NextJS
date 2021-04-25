21b2b1dae9278455d6d2cabc6d3ffa687348444b
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-26 01:02:56 +0200

    all-posts index page

--- travel-blog-nextjs/pages/posts/index.js
+++ travel-blog-nextjs/pages/posts/index.js
@@ -1,7 +1,15 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {title: 'Cairo', country: 'Egypt', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'egypt', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."},
+    {title: 'Ecuador', country: 'Ecuador', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'ecuador', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."},
+    {title: 'Thailand', country: 'Thailand', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'thailand', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."}
+  ]
+
 function AllPostsPage(){
     return (
         <div>
-            All Posts
+            <AllPosts posts={DUMMY_POSTS}/>
         </div>
     )
 }
