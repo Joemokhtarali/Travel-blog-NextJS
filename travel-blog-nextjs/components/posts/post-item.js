958f6769a51165ca556809700341f8fd0b55ef29
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-25 17:24:43 +0200

    added post-item and styled it

--- travel-blog-nextjs/components/posts/post-item.js
+++ travel-blog-nextjs/components/posts/post-item.js
@@ -1,24 +1,37 @@
-import Link from 'next/link'
+import Link from "next/link";
 import Image from "next/image";
-import classes from './post-item.module.css'
+import classes from "./post-item.module.css";
 
-function PostItem(){
-    return (
-        <li className={classes.post}>
-            <Link>
-            <a>
-                <div className={classes.image}>
-                    <Image src='' alt='' height={} width={} />
-                </div>
-                <div className={classes.content}>
-                    <h3>Title</h3>
-                    <time>July 13h 2022</time>
-                    <p>The Blog</p>
-                </div>
-            </a>
-            </Link>
-        </li>
-    )
+function PostItem(props) {
+  const { title, country, date, image, content, youtube_link, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+  })
+
+  const imagePath = `/images/posts/cairo.jpeg`
+  const linkPath = `/posts/${slug}`
+
+  return (
+    <li className={classes.post}>
+      <Link href={linkPath}>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} height={300} width={300} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{content}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
-export default PostItem 
+export default PostItem;
+
+// Country, date, image, content, youtube link, slug,
--- travel-blog-nextjs/pages/index.js
+++ travel-blog-nextjs/pages/index.js
@@ -2,11 +2,17 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/Hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+  {title: 'Cairo', country: 'Egypt', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'egypt-cairo1', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."},
+  {title: 'Cairo', country: 'Egypt', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'egypt-cairo2', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."},
+  {title: 'Cairo', country: 'Egypt', date: '07/13/2020', image: 'cairo.jpeg', youtube_link: 'https://www.youtube.com/watch?v=0NjnKn63r8w', slug: 'egypt-cairo3', content: "Whenever you hear Egypt, Your mind will always imagine three Pyramids, The Sphinx, Camels, and Horses. This picture isn't wrong, I'll be happy to write about it in this article that will make you more eager to visit Egypt. And I will write about everything you'll need to know before visiting The land of the Pharaohs."}
+]
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS}/>
     </Fragment>
   );
 }
