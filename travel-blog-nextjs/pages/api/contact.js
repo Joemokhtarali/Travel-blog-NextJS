a3633574082368ee799cca2901c109cca35ad171
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-05-06 18:26:28 +0200

    adding contact API handler function to check incoming data

---
+++ travel-blog-nextjs/pages/api/contact.js
@@ -0,0 +1,28 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({message: 'invalid input'})
+      return;
+    }
+
+    //store it in a database
+    const newMessage = {
+      email,
+      name,
+      message
+    }
+    console.log(newMessage)
+    res.status(201).json( {message: 'success', message: newMessage} )
+  }
+}
+
+export default handler
