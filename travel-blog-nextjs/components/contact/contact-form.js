e5aea7e7bced39c220e7f8bcbb43f6387574a136
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-05-06 20:00:13 +0200

    switching form to controlled form

--- travel-blog-nextjs/components/contact/contact-form.js
+++ travel-blog-nextjs/components/contact/contact-form.js
@@ -1,20 +1,20 @@
 import classes from "./contact-form.module.css";
-import { useState } from 'react'
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 function ContactForm() {
-    const [enteredEmail, setEnteredEmail] = useState('')
-    const [enteredName, setEnteredName] = useState('')
-    const [enteredMessage, setEnteredMessage] = useState('')
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
 
-    function sendMessageHandler(event) {
-        event.preventDefault()
-        axios.post(
-            '/api/contact',
-            { email, name, message },
-            { options }
-          );
-    }
+  function sendMessageHandler(event) {
+    event.preventDefault();
+    axios.post("/api/contact", {
+      email: enteredEmail,
+      name: enteredName,
+      message: enteredMessage,
+    });
+  }
 
   return (
     <section className={classes.contact}>
@@ -23,18 +23,39 @@ function ContactForm() {
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email"> Your Email</label>
-            <input type="email" id="email" required placeholder='name' onChange={setEnteredEmail}/>
+            <input
+              type="email"
+              id="email"
+              required
+              placeholder="name"
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
 
           <div className={classes.control}>
             <label htmlFor="name"> Your Name</label>
-            <input type="text" id="name" required placeholder='email' onChange={setEnteredName}/>
+            <input
+              type="text"
+              id="name"
+              required
+              placeholder="email"
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
 
         <div className={classes.control}>
           <label htmlFor="message"> Your Message</label>
-          <textarea id="message" rows="5" placeholder='write your message here ....' onChange={setEnteredMessage}/>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            placeholder="write your message here ...."
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
         </div>
 
         <div className={classes.actions}>
